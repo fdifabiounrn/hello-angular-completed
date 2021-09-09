da5e7589e30a54d17434f4407cf168e86efb0c98
federicoezequieldifaio <federicoezequieldifabio@gmail.com>
2021-09-08 22:18:29 -0300

    angular material

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,15 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent {
+  @Input() isExpanded: boolean | undefined;
+
+  public routeLinks = [
+    {link: "person", name: "Personas", icon: "people"},
+    {link: "buses", name: "Colectivos", icon: "directions_bus"},
+  ];
+}
